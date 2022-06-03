8b3d645fe7c65b49d754f6ced954136570712110
shourovahmd <moh.abuhanifa@gmail.com>
2022-06-04 01:01:01 +0600

    removed extra

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Component/Home';
 
 function App() {
   return (
     <div>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+      </Routes>
     </div>
   );
 }
