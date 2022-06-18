62f063eddfe3ca7a3cf63c3275f6f69daf5328aa
shourovahmd <moh.abuhanifa@gmail.com>
2022-06-19 00:35:43 +0600

    profilemodel added

---
+++ src/Component/miscellaneous/ProfileModel.js
@@ -0,0 +1,10 @@
+
+const ProfileModel = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default ProfileModel;
--- src/Component/miscellaneous/SideDrawer.js
+++ src/Component/miscellaneous/SideDrawer.js
@@ -1,10 +1,58 @@
+import { BellIcon, ChevronDownIcon } from "@chakra-ui/icons";
+import {
+  Avatar,
+  Button,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Text,
+  Tooltip
+} from "@chakra-ui/react";
+import { useState } from "react";
+import { ChatState } from "../../Context/ChatProvider";
 
 const SideDrawer = () => {
-    return (
+  const [search, setSearch] = useState("");
+  const [searchResult, setSearchResult] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [loadingChat, setLoadingChat] = useState(false);
+  const { user } = ChatState();
+  return (
+    <>
+      <div className="drawer">
+        <Tooltip label="Search Users to chat" hasArrow placement="bottom-end">
+          <Button variant="ghost">
+            <i className="fas fa-search"></i>
+            <Text d={{ base: "none", md: "flex" }}>Search User</Text>
+          </Button>
+        </Tooltip>
+        <Text>Tell and Talk</Text>
         <div>
-            SideDrawer
+          <Menu>
+            <MenuButton>
+              <BellIcon></BellIcon>
+            </MenuButton>
+            <MenuList></MenuList>
+          </Menu>
+          <Menu>
+            <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+              <Avatar
+                size="sm"
+                cursor="pointer"
+                name={user.name}
+                src={user.pic}
+              ></Avatar>
+            </MenuButton>
+            <MenuList>
+              <MenuItem>My Profile</MenuItem>
+              <MenuItem>Logout</MenuItem>
+            </MenuList>
+          </Menu>
         </div>
-    );
+      </div>
+    </>
+  );
 };
 
 export default SideDrawer;
