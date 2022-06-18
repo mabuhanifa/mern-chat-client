e04cf002a0da1b665171a17e5abaf51bec13a89d
shourovahmd <moh.abuhanifa@gmail.com>
2022-06-19 00:37:41 +0600

    profilemodal added

--- src/Component/miscellaneous/ProfileModal.js
+++ src/Component/miscellaneous/ProfileModal.js
@@ -1,5 +1,5 @@
 
-const ProfileModel = () => {
+const ProfileModal = () => {
     return (
         <div>
             
@@ -7,4 +7,4 @@ const ProfileModel = () => {
     );
 };
 
-export default ProfileModel;
+export default ProfileModal;
